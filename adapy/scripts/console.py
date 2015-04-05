4745ab51d62d5987e03848c91f35e4cee9b8b455
Michael Koval <mkoval@cs.cmu.edu>
2015-04-05 01:43:36 -0400

    Added console.py to AdaPy, to mimic HerbPy.

---
+++ adapy/scripts/console.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""
+Provides a simple console that sets up basic functionality for 
+using AdaPy and openravepy.
+"""
+
+import IPython
+import adapy
+import argparse
+import logging
+import numpy
+import openravepy
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='utility script for loading AdaPy')
+    parser.add_argument('-s', '--sim', action='store_true',
+                        help='simulation mode')
+    parser.add_argument('-v', '--viewer', nargs='?', const=True,
+                        help='attach a viewer of the specified type')
+    parser.add_argument('--robot-xml', type=str,
+                        help='robot XML file; defaults to herb_description')
+    parser.add_argument('--env-xml', type=str,
+                        help='environment XML file; defaults to an empty environment')
+    parser.add_argument('--debug', action='store_true',
+                        help='enable debug logging')
+    args = parser.parse_args()
+
+    openravepy.RaveInitialize(True)
+    openravepy.misc.InitOpenRAVELogging()
+
+    if args.debug:
+        openravepy.RaveSetDebugLevel(openravepy.DebugLevel.Debug)
+
+    env, robot = adapy.initialize(
+        sim=args.sim,
+        attach_viewer=args.viewer,
+        robot_xml=args.robot_xml,
+        env_path=args.env_xml
+    )
+
+    IPython.embed()
