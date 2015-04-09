63ab7f5f28c95dd03a2ddf27e9976365f334be0a
Michael Koval <mkoval@cs.cmu.edu>
2015-04-08 23:07:55 -0400

    Cleaning up __init__.

--- adapy/src/adapy/__init__.py
+++ adapy/src/adapy/__init__.py
@@ -1,101 +1,56 @@
 #!/usr/bin/env python
-PACKAGE = 'adapy'
 import logging, prpy, openravepy, adapy
 
-logger = logging.getLogger('adapy')
+URDF_PATH = 'robots/mico-modified-updated.urdf'
+URDF_PACKAGE = 'ada_description'
+SRDF_PATH = ''
+SRDF_PACKAGE = 'ada_description'
 
-# Add dependencies to our OpenRAVE plugin and data search paths.
+def initialize(env_path=None, attach_viewer=False, **kw_args):
+    from adarobot import ADARobot
+    from util import find_adapy_resource
 
-
-def initialize(robot_xml=None, env_path=None, attach_viewer=False, load_xml = True, **kw_args):
     prpy.logger.initialize_logging()
 
     # Create the environment.
     env = openravepy.Environment()
     if env_path is not None:
-      if not env.Load(env_path):
-        raise Exception('Unable to load environment frompath %s' % env_path)
+        if not env.Load(env_path):
+            raise IOError(
+                'Unable to load environment "{:s}".'.format(env_path))
 
-    if load_xml is True:
-      if robot_xml is None:
-        import os, rospkg
-        rospack = rospkg.RosPack()
-        base_path = rospack.get_path('ada_description')
+    # Load ADA from URDF.
+    urdf_path = find_adapy_resource(URDF_PATH, package=URDF_PACKAGE)
+    if urdf_path is None:
+        raise AdaPyException('Failed loading URDF "{:s}". Do you have the'
+                             ' package {:s} installed?'.format(
+                                URDF_PATH, URDF_PACKAGE))
 
-        robot_xml = os.path.join(base_path, 'ordata', 'robots', 'mico-modified.robot.xml')
+    srdf_path = ''
+    if srdf_path is None:
+        raise AdaPyException('Failed loading SRDF "{:s}". Do you have the'
+                             ' package {:s} installed?'.format(
+                                SRDF_PATH, SRDF_PACKAGE))
 
-      robot = env.ReadRobotXMLFile(robot_xml)
-      env.Add(robot)
-    else:
-      if prpy.dependency_manager.is_catkin():
-            # Find the HERB URDF and SRDF files.
-          from catkin.find_in_workspaces import find_in_workspaces
-          share_directories = find_in_workspaces(search_dirs=['share'],
-                                                   project='ada_description')
-          if not share_directories:
-              logger.error('Unable to find the Ada model. Do you have the'
-                             ' package ada installed?')
-              raise ValueError('Unable to find Ada model.')
+    # Use or_urdf to load ADA from URDF and SRDF.
+    with env:
+        or_urdf = openravepy.RaveCreateModule(env, 'urdf')
+        ada_name = or_urdf.SendCommand(
+            'load {:s} {:s}'.format(urdf_path, srdf_path))
 
+    robot = env.GetRobot(ada_name)
+    if robot is None:
+        raise AdaPyException('Failed loading ADA with or_urdf.')
 
-          import os.path, rospkg
-          found_models = False
-          rospack = rospkg.RosPack()
-          base_path = rospack.get_path('ada_description')
-
-          for share_directory in share_directories:
-
-              urdf_path = os.path.join(base_path, 'robots', 'mico-modified-updated.urdf')
-              srdf_path = os.path.join(base_path, 'robots', 'mico-modified.srdf')
-
-              if os.path.exists(urdf_path) and os.path.exists(srdf_path):
-                  found_models = True
-                  break
-            #from IPython import embed
-            #embed()
-          if not found_models:
-              logger.error('Missing URDF file for ADA.'
-                           ' Is the ada_description package properly installed?')
-              raise ValueError('Unable to find ADA URDF file.')
-
-            # Load the URDF file into OpenRAVE.
-
-          urdf_module = openravepy.RaveCreateModule(env, 'urdf')
-          if urdf_module is None:
-              logger.error('Unable to load or_urdf module. Do you have or_urdf'
-                             ' built and installed in one of your Catkin workspaces?')
-              raise ValueError('Unable to load or_urdf plugin.')
-
-
-          args = 'Load {:s} {:s}'.format(urdf_path, srdf_path)
-            # ada_name = urdf_module.SendCommand(args)
-            # if ada_name is None:
-            #     raise ValueError('Failed loading ADA model using or_urdf.')
-            #rom IPython import embed
-            #embed()
-          name = urdf_module.SendCommand(args)
-          robot = env.GetRobot(name)
-          if robot is None:
-              raise ValueError('Unable to find robot with name "{:s}".'.format(
-                                 ada_name))   
-    # Default arguments.
-    #keys = [ 'sim']
-    #for key in keys:
-     #   if key not in kw_args:
-     #      kw_args[key] = sim
-    
-    from adarobot import ADARobot
+    # Bind AdaPy-specific functionality on the robot.
     prpy.bind_subclass(robot, ADARobot, **kw_args)
 
-#     from r2robot import R2Robot
-#     prpy.bind_subclass(robot, R2Robot, **kw_args)
-
-#     # Start by attempting to load or_rviz.
+    # Start by attempting to load or_rviz.
     if attach_viewer == True:
         attach_viewer = 'rviz'
         env.SetViewer(attach_viewer)
 
-#         # Fall back on qtcoin if loading or_rviz failed
+        # Fall back on qtcoin if loading or_rviz failed
         if env.GetViewer() is None:
            logger.warning('Loading rviz failed. Falling back on qt_coin.')
            attach_viewer = 'qtcoin'
@@ -103,11 +58,12 @@ def initialize(robot_xml=None, env_path=None, attach_viewer=False, load_xml = Tr
     if attach_viewer and env.GetViewer() is None:
         env.SetViewer(attach_viewer)
         if env.GetViewer() is None:
-            raise Exception('Failed creating viewer of type "{0:s}".'.format(attach_viewer))
+            raise AdaPyException(
+                'Failed creating viewer of type "{:s}".'.format(attach_viewer))
      
-#     # Remove the ROS logging handler again. It might have been added when we loaded or_rviz.
+    # Remove the ROS logging handler again. It might have been added when we
+    # loaded or_rviz.
     prpy.logger.remove_ros_logger()
-    #print "hello!!!!!************************"
 
     return env, robot
 
--- adapy/src/adapy/adarobot.py
+++ adapy/src/adapy/adarobot.py
@@ -12,28 +12,11 @@ TSR_PATHS = [
     'config/glass_move_tsr.yaml',
 ]
 
-
-def find_adapy_resource(relative_path):
-    from catkin.find_in_workspaces import find_in_workspaces
-
-    paths = find_in_workspaces(project='adapy', search_dirs=['share'],
-                               path=relative_path, first_match_only=True)
-
-    if paths and len(paths) == 1:
-        return paths[0]
-    else:
-        raise IOError('Loading AdaPy resource "{:s}" failed.'.format(
-                      relative_path))
-
-
 class ADARobot(Robot):
     def __init__(self, sim):
-<<<<<<< HEAD
         from prpy.base.mico import Mico
         from prpy.base.micohand import MicoHand
-=======
-        MicoRobot.__init__(self, robot_name = 'ADA')
->>>>>>> master
+        from util import AdaPyException, find_adapy_resource
 
         # We need to hard-code the name. Otherwise, it defaults to
         # "mico-modified".
@@ -44,15 +27,12 @@ class ADARobot(Robot):
         self.arm.hand = self.arm.GetEndEffector()
         self.manipulators = [ self.arm ]
 
-<<<<<<< HEAD
         # Bind robot-specific subclasses.
         prpy.bind_subclass(self.arm, Mico, sim=sim)
         prpy.bind_subclass(self.arm.hand, MicoHand, sim=sim,
                            manipulator=self.arm)
 
         # TODO: Load an IdealController controller in simulation.
-=======
->>>>>>> master
 
         # Load default named configurations from YAML.
         self.configurations.add_group('arm', self.arm.GetIndices())
@@ -60,7 +40,6 @@ class ADARobot(Robot):
             self.configurations.load_yaml(
                 find_adapy_resource(CONFIGURATIONS_PATH))
         except IOError as e:
-<<<<<<< HEAD
             logger.warning('Failed loading named configurations from "%s": %s',
                            configurations_path, e.message)
 
@@ -77,43 +56,6 @@ class ADARobot(Robot):
         # Initialize the default planning pipeline.
         from prpy.planning import Sequence, Ranked, FirstSupported
         from prpy.planning import (
-=======
-            logger.warning('Failed loading named configurations from "%s".', configurations_path)
-
-
-        if self.tsrlibrary is not None:
-            tsrPaths = ['config/glass_grasp_tsr.yaml', 'config/glass_move_tsr.yaml']
-            for tsrPath in tsrPaths:
-              if prpy.dependency_manager.is_catkin():
-                  from catkin.find_in_workspaces import find_in_workspaces
-                  tsrs_paths = find_in_workspaces(search_dirs=['share'], project='adapy',
-                                 path=tsrPath, first_match_only=True)
-                  if not tsrs_paths:
-                    raise ValueError('Unable to load named tsrs from path "%s".'. tsrPath)
-
-                  tsrs_path = tsrs_paths[0]
-              else:
-                  tsrs_path = os.path.join(package_path, tsrPath)
-
-              try:
-                self.tsrlibrary.robot_name = 'ADA' #need to hardcode this, otherwise the name becomes mico-modified
-                self.tsrlibrary.load_yaml(tsrs_path)
-                #self.tsrlibrary.load_yaml(tsrs_path)
-              except IOError as e:
-                  raise ValueError('Failed loading named tsrs from "{:s}".'.format(
-                    tsrs_path))
-
-
-
-        # Initialize a default planning pipeline.
-        from prpy.planning import (
-            FirstSupported,
-            MethodMask,
-            Ranked,
-            Sequence,
-        )
-        from prpy.planning import (
->>>>>>> master
             BiRRTPlanner,
             CBiRRTPlanner,
             CHOMPPlanner,
@@ -126,7 +68,6 @@ class ADARobot(Robot):
             VectorFieldPlanner
         )
 
-<<<<<<< HEAD
         self.snap_planner = SnapPlanner()
         self.cbirrt_planner = CBiRRTPlanner()
         self.vectorfield_planner = VectorFieldPlanner()
@@ -140,68 +81,6 @@ class ADARobot(Robot):
             actual_planner,
             NamedPlanner(delegate_planner=actual_planner)
         )
-=======
-        # TODO: These should be meta-planners.
-        self.named_planner = NamedPlanner()
-        self.ik_planner = IKPlanner()
-
-        # Special-purpose planners.
-        self.snap_planner = SnapPlanner()
-        self.vectorfield_planner = VectorFieldPlanner()
-        self.greedyik_planner = GreedyIKPlanner()
-
-        # General-purpose planners.
-        self.birrt_planner = BiRRTPlanner()
-        self.cbirrt_planner = CBiRRTPlanner()
-
-        actual_planner = Sequence(
-        #     # First, try the straight-line trajectory.
-              self.snap_planner,
-        #     # Then, try a few simple (and fast!) heuristics.
-              self.vectorfield_planner,
-              self.greedyik_planner,
-        #     # Next, try a trajectory optimizer.
-        #     #self.trajopt_planner or self.chomp_planner,
-        #     # If all else fails, call an RRT.
-             self.birrt_planner,
-             MethodMask(
-                 FirstSupported(
-        #             # Try sampling the TSR and planning with BiRRT. This only
-        #             # works for PlanToIK and PlanToTSR with strictly goal TSRs.
-                     TSRPlanner(delegate_planner=self.birrt_planner),
-        #             # Fall back on CBiRRT, which also handles start and
-        #             # constraint TSRs.
-                     self.cbirrt_planner,
-                 ),
-                 methods=['PlanToIK', 'PlanToTSR', 'PlanToEndEffectorPose', 'PlanToEndEffectorOffset']
-             )
-         )
-        self.planner = FirstSupported(
-            actual_planner,
-             # Special purpose meta-planner.
-             NamedPlanner(delegate_planner=actual_planner),
-        )
-       # self.planner = self.cbirrt_planner
-        #from herbpy.action import *
-        from adapy.tsr import * 
-    """
-    def ExecuteTrajectory(self, traj, retime=True, **kw_args):
-        if retime:
-            # Smooth the trajectory using quadratic interpolation.
-            result = openravepy.planningutils.SmoothTrajectory(traj, 0.95, 0.95, '', '')
-            if result != openravepy.PlannerStatus.HasSolution:
-                raise ValueError('Retiming trajectory failed.')
-
-            import prpy.rave
-            num_fixed = prpy.rave.fix_trajectory(traj)
-            if num_fixed > 0:
-                logger.warning('Removed %d invalid waypoints from trajectory.', num_fixed)
-
-        self.GetController().SetPath(traj)
-        return traj
-    """
-  
->>>>>>> master
 
     def CloneBindings(self, parent):
         super(ADARobot, self).CloneBindings(parent)
