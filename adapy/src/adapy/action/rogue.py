1040e882aa656676e66fbbad42e73623d9991237
Rachel <rmh@andrew.cmu.edu>
2015-12-25 01:37:31 -0500

    refactor rogue to new style

--- adapy/src/adapy/action/rogue.py
+++ adapy/src/adapy/action/rogue.py
@@ -8,6 +8,79 @@ import numpy, cPickle, time, os.path
 logger = logging.getLogger('adapy')
 
 @ActionMethod
+def PointAt(robot, focus, manip=None, render=False):
+    """
+    @param robot The robot performing the point
+    @param focus The 3-D coordinate in space or object
+                 that is being pointed at
+    @param manip The manipulator to perform the point with.
+    @param render Render tsr samples during planning
+    """
+    env = robot.GetEnv()
+    pointing_coord = GetPointFrom(env, focus)
+    return Point(robot, pointing_coord, manip, render)
+
+@ActionMethod
+def PresentAt(robot, focus, manip=None, render=True):
+    """
+    @param robot The robot performing the presentation
+    @param focus The 3-D coordinate in space or object that
+                 is being presented
+    @param manip The manipulator to perform the presentation with.
+    @param render Render tsr samples during planning
+    """
+    env = robot.GetEnv()
+    presenting_coord = GetPointFrom(env, focus)
+    return Present(robot, presenting_coord, manip, render)
+
+@ActionMethod
+def SweepAt(robot, start, end, manip=None, margin=0.3, render=True):
+    """
+    @param robot The robot performing the sweep
+    @param start The object or 3-D position that marks the start
+    @param end The object or 3-D position that marks the end
+    @param manip The manipulator to perform the sweep
+    @param margin The distance between the start object and the hand,
+                  so the vertical space between the hand and objects.
+                  This must be enough to clear the objects themselves.
+    @param render Render tsr samples during planning
+    """
+    env = robot.GetEnv()
+    start_coord = GetPointFrom(env, start)
+    end_coord = GetPointFrom(env, end)
+    return Sweep(robot, start_coord, end_coord, manip, margin, render) 
+
+def GetPointFrom(env, focus):
+    """
+    param env The environment where the item exists
+    param focus THe area to be referred to
+    """
+    #Pointing at an object
+    if isinstance(focus, (openravepy.KinBody, openravepy.KinBody.Link)):
+        with env: 
+            focus_trans = focus.GetTransform()
+        coord = list(focus_trans[0:3, 3])
+
+    #Pointing at a point in space as numpy array
+    elif (isinstance(focus, numpy.ndarray) and (focus.ndim == 1) 
+           and (len(focus) == 3)):
+        coord = list(focus)
+
+    #Pointing at point in space as 4x4 transform
+    elif isinstance(focus, numpy.ndarray) and (focus.shape == (4, 4)):
+        coord = list(focus[0:3, 3])
+
+    #Pointing at a point in space as list or tuple
+    elif (isinstance(focus, (tuple, list)) and len(focus) == 3):
+        coord = focus
+
+    else:
+        raise prpy.exceptions.PrPyException('Focus of the point is an \
+                unknown object')
+
+    return coord
+
+@ActionMethod
 def Point(robot, focus, manip=None, render=False):
     """
     @param robot The robot performing the point
@@ -17,32 +90,21 @@ def Point(robot, focus, manip=None, render=False):
                  This must be the right arm
     @param render Render tsr samples during planning
     """
-
-    #Pointing at an object
-    if type(focus) == openravepy.openravepy_int.KinBody:
-        focus_trans = focus.GetTransform()
-        goal_name = focus.GetName()
-    #Pointing at a point in space
-    elif (type(focus) == numpy.ndarray) and (focus.ndim == 1):
-        if len(focus) != 3:
-            raise prpy.exceptions.PrPyExceptions('A point in space must \
-                    contain exactly three coordinates')
-        focus_trans = numpy.eye(4)
-        focus_trans[0:3, 3] = focus
-        goal_name = None
-    #Possible feature: if many objects then pass to multipoint
-    else:
-        raise prpy.exceptions.PrPyException('Focus of the point is an \
-                unknown object')
-
     if manip is None:
-        manip = robot.arm
-        manip.SetActive()
+        manip = robot.GetActiveManipulator()
 
-    point_tsr = robot.tsrlibrary(None, 'point', focus_trans, manip)
+    focus_trans = numpy.eye(4, dtype='float')
+    focus_trans[0:3, 3] = coord
 
-    with prpy.viz.RenderTSRList(point_tsr, robot.GetEnv(), render=render):
-        robot.PlanToTSR(point_tsr, execute=True)
+    with robot.GetEnv():
+        point_tsr = robot.tsrlibrary(None, 'point', focus_trans, manip)
+
+    p = openravepy.KinBody.SaveParameters
+    with robot.CreateRobotStateSaver(p.ActiveManipulator | p.ActiveDOF):
+        robot.SetActiveManipulator(manip)
+        robot.SetActiveDOFs(manip.GetArmIndices())
+        with prpy.viz.RenderTSRList(point_tsr, robot.GetEnv(), render=render):
+            robot.PlanToTSR(point_tsr, execute=True)
    
     #Should be closehand but that doesnt work
     robot.arm.hand.MoveHand(f1=0.9, f2=0.9)
@@ -57,29 +119,22 @@ def Present(robot, focus, manip=None, render=True):
                  This must be the right arm.
     @param render Render tsr samples during planning
     """
-    #Presenting an object
-    if type(focus) == openravepy.openravepy_int.KinBody:
-        focus_trans = focus.GetTransform()
-    #Presenting a point in space
-    elif (type(focus) == numpy.ndarray) and (focus.ndim == 1):
-        if len(focus) != 3:
-            raise prpy.exceptions.PrPyExceptions('A presentation in space \
-                    must contain exactly three coordinates')
-        focus_trans = numpy.eye(4)
-        focus_trans[0:3, 3] = focus
-    else:
-        raise prpy.exceptions.PrPyException('Focus of the presentation is an \
-                unknown object')
-
     if manip is None:
-        manip = robot.arm
-        manip.SetActive()
+        manip = robot.GetActiveManipulator()
 
-    present_tsr = robot.tsrlibrary(None, 'present', focus_trans, manip)
+    focus_trans = numpy.eye(4, dtype='float')
+    focus_trans[0:3, 3] = coord
+
+    with robot.GetEnv():
+        present_tsr = robot.tsrlibrary(None, 'present', focus_trans, manip)
     
-    with prpy.viz.RenderTSRList(present_tsr, robot.GetEnv(), render=render):
-        robot.PlanToTSR(present_tsr, execute=True)
-    
+    p = openravepy.KinBody.SaveParameters
+    with robot.CreateRobotStateSaver(p.ActiveManipulator | p.ActiveDOF):
+        robot.SetActiveManipulator(manip)
+        robot.SetActiveDOFs(manip.GetArmIndices())
+        with prpy.viz.RenderTSRList(present_tsr, robot.GetEnv(), render=render):
+            robot.PlanToTSR(present_tsr, execute=True)   
+
     #should be closehand() but that doesnt work
     robot.arm.hand.MoveHand(f1=0.9, f2=0.9)
 
@@ -95,32 +150,6 @@ def Sweep(robot, start, end, manip=None, margin=0.3, render=True):
                   This must be enough to clear the objects themselves.
     @param render Render tsr samples during planning
     """
-
-    if type(start) == openravepy.openravepy_int.KinBody: 
-        start_trans = start.GetTransform()
-        start_coords = start_trans[0:3, 3]
-    elif (type(start) == numpy.ndarray) and (start.ndim == 1):
-        if len(start) != 3:
-            raise prpy.exceptions.PrPyExceptions('A point in space must \
-                    contain exactly three coordinates')
-        start_coords = start
-    else: 
-        raise prpy.exceptions.PrPyException('Focus of the point is an \
-                unknown object')
-
-    if type(end) == openravepy.openravepy_int.KinBody:
-        end_trans = end.GetTransform()
-        end_coords = end_trans[0:3, 3]
-    elif (type(end) == numpy.ndarray) and (end.ndim == 1):
-        if len(end) != 3:
-            raise prpy.exceptions.PrPyExceptions('A point in space must \
-                    contain exactly three coordinates')
-        end_coords = end
-        end_trans = numpy.eye(4)
-        end_trans[0:3, 3] = end_coords
-    else:
-        raise prpy.exceptions.PrPyException('Focus of the point is an \
-                unknown object')
 
     if manip is None:
         manip = robot.GetActiveManipulator()
@@ -138,15 +167,27 @@ def Sweep(robot, start, end, manip=None, margin=0.3, render=True):
         raise prpy.exceptions.PrPyException('Manipulator does not have an \
                  associated hand')
 
+    end_trans = numpy.eye(4, dtype='float')
+    end_trans[0:3, 3] = end_coords
+ 
     #Should be close hand but that is broken
     hand.MoveHand(f1=0.9, f2=0.9)
-    manip.PlanToEndEffectorPose(hand_pose)
+    q = openravepy.KinBody.SaveParameters
+    with robot.CreateRobotStateSaver(q.ActiveManipulator | q.ActiveDOF):
+        robot.SetActiveManipulator(manip)
+        robot.SetActiveDOFs(manip.GetArmIndices())
+        manip.PlanToEndEffectorPose(hand_pose)
     
     #TSR to sweep to end position
-    sweep_tsr = robot.tsrlibrary(None, 'sweep', end_trans, manip)
+    with robot.GetEnv():
+        sweep_tsr = robot.tsrlibrary(None, 'sweep', end_trans, manip)
 
-    with prpy.viz.RenderTSRList(sweep_tsr, robot.GetEnv(), render=render):
-        robot.PlanToTSR(sweep_tsr, execute=True)
+    p = openravepy.KinBody.SaveParameters
+    with robot.CreateRobotStateSaver(p.ActiveManipulator | p.ActiveDOF):
+        robot.SetActiveManipulator(manip)
+        robot.SetActiveDOFs(manip.GetArmIndices())
+        with prpy.viz.RenderTSRList(sweep_tsr, robot.GetEnv(), render=render):
+             robot.PlanToTSR(sweep_tsr, execute=True)
 
 @ActionMethod
 def Exhibit(robot, obj, manip=None, distance=0.1, wait=2, render=True):
@@ -159,21 +200,25 @@ def Exhibit(robot, obj, manip=None, distance=0.1, wait=2, render=True):
     @param render Render tsr samples during planning
     """
 
+    with robot.GetEnv():
+        if manip is None:
+            manip = robot.GetActiveManipulator()
     robot.Grasp(obj)
 
-    #Lift the object - write more tsrs
-    lift_tsr = robot.tsrlibrary(obj, 'lift', manip, distance=distance)
-    
-    with prpy.viz.RenderTSRList(lift_tsr, robot.GetEnv(), render=render):
-        robot.PlanToTSR(lift_tsr, execute=True)
+    p = openravepy.KinBody.SaveParameters
+    with robot.CreateRobotStateSaver(p.ActiveManipulator | p.ActiveDOF):
+        robot.SetActiveManipulator(manip)
+        robot.SetActiveDOFs(manip.GetArmIndices())    
+        
+        #Lift the object
+        lift_tsr = robot.tsrlibrary(obj, 'lift', manip, distance=distance)
+        with prpy.viz.RenderTSRList(lift_tsr, robot.GetEnv(), render=render):
+            robot.PlanToTSR(lift_tsr, execute=True)
 
-    #Wait for 'time'
-    time.sleep(wait)
+        #Wait for 'time'
+        time.sleep(wait)
 
-    #'Unlift' the object, so place it back down
-    unlift_tsr = robot.tsrlibrary(obj, 'lift', manip, distance=-distance)
-    
-    with prpy.viz.RenderTSRList(unlift_tsr, robot.GetEnv(), render=render):
-        robot.PlanToTSR(unlift_tsr, execute=True)
-
-
+        #'Unlift' the object, so place it back down
+        unlift_tsr = robot.tsrlibrary(obj, 'lift', manip, distance=-distance)
+        with prpy.viz.RenderTSRList(unlift_tsr, robot.GetEnv(), render=render):
+            robot.PlanToTSR(unlift_tsr, execute=True)
