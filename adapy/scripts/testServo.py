44e2f696b75f3f9b3c12be434c5303291b584a59
Stefanos Nikolaidis <snikolaidis1983@gmail.com>
2016-07-29 02:41:02 -0400

    adding testServo.py

---
+++ adapy/scripts/testServo.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+import adapy, openravepy, numpy, prpy, rospy
+from IPython import embed
+from std_msgs.msg import Float64
+
+def PlanToTransform(env, robot, transform):
+    handle = openravepy.misc.DrawAxes(env, transform);
+    iksolver = robot.arm.GetIkSolver()
+    param = openravepy.IkParameterization(transform, openravepy.IkParameterizationType.Transform6D)
+    solution = iksolver.Solve(param, robot.GetActiveDOFValues(),  0)
+    traj =  robot.PlanToConfiguration(solution.GetSolution())
+    return traj;
+
+def PlanToOffset(env, robot, offset):
+    transform = robot.arm.GetEndEffectorTransform()
+    transform[0:3, 3] += offset;
+    traj = PlanToTransform(env, robot, transform);
+    return traj
+
+rospy.init_node('test_scenario', anonymous = True)
+openravepy.RaveInitialize(True, level=openravepy.DebugLevel.Debug)
+openravepy.misc.InitOpenRAVELogging();
+rospy.init_node('test_scenario', anonymous = True)
+
+env, robot = adapy.initialize(attach_viewer='qtcoin', sim=True)
+manip = robot.arm
+
+
+
+manip.SetIkSolver(openravepy.RaveCreateIkSolver(env, 'NloptIK'))
+#robot.SetActiveDOFs([2,3,4,5,6,7])
+controller_j1_name = 'vel_j1_controller'
+controller_j2_name = 'vel_j2_controller'
+controller_j3_name = 'vel_j3_controller'
+controller_j4_name = 'vel_j4_controller'
+controller_j5_name = 'vel_j5_controller'
+controller_j6_name = 'vel_j6_controller'
+
+command_j1_topic_name = '{:s}/command'.format(controller_j1_name)
+command_j2_topic_name = '{:s}/command'.format(controller_j2_name)
+command_j3_topic_name = '{:s}/command'.format(controller_j3_name)
+command_j4_topic_name = '{:s}/command'.format(controller_j4_name)
+command_j5_topic_name = '{:s}/command'.format(controller_j5_name)
+command_j6_topic_name = '{:s}/command'.format(controller_j6_name)
+jointstate_topic_name = 'joint_states'
+
+velocity_j1_publisher = rospy.Publisher(command_j1_topic_name, Float64)
+velocity_j2_publisher = rospy.Publisher(command_j2_topic_name, Float64)
+velocity_j3_publisher = rospy.Publisher(command_j3_topic_name, Float64)
+velocity_j4_publisher = rospy.Publisher(command_j4_topic_name, Float64)
+velocity_j5_publisher = rospy.Publisher(command_j5_topic_name, Float64)
+velocity_j6_publisher = rospy.Publisher(command_j6_topic_name, Float64)
+
+twist = numpy.array([0,1,0,0,0,0])
+joint_velocities =  prpy.util.ComputeJointVelocityFromTwist(robot,twist)
+joint_velocities = joint_velocities[0]
+while True:
+  robot.arm.Servo(joint_velocities)
+
+
+
+from IPython import embed
+embed()
