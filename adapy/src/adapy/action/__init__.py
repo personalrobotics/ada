ca220aba761c168f0adac25e18ff39b3e19a301e
Laura Herlant <herlant@cmu.edu>
2017-06-20 14:07:15 -0400

    Added action for Skewering from a plate

--- adapy/src/adapy/action/__init__.py
+++ adapy/src/adapy/action/__init__.py
@@ -1,2 +1,3 @@
 from grasping import Grasp
 from rogue import *
+from eating import SkewerFromPlate
---
+++ adapy/src/adapy/action/eating.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+import logging, numpy
+from prpy.action import ActionMethod, ActionError
+from prpy.planning.base import PlanningError
+logger = logging.getLogger('adapy')
+import prpy
+import IPython
+from tf.transformations import *
+
+@ActionMethod
+def SkewerFromPlate(robot, goal_position, z_offset=None, plate=None, fork=None, manip=None):
+	"""
+	@param robot The robot performing the skewering
+	@param plate the plate kinbody
+	@param goal_position the (x,y) position of the bite w.r.t. to the center 
+		of the place
+	@param z_offset the height from the bottom of the plate (i.e. plate 
+		thickness + desired safety margin) - if None the height of the
+		provided plate kinbody is used
+	@param fork fork kinbody - if None, will be read from robot's env
+	@param manip robot manipulator - if None will use the active one
+	"""
+	
+	# Get the fork kinbody
+	env = robot.GetEnv()
+	if fork is None:
+		fork = env.GetKinBody('fork')
+		if fork is None:
+			raise ActionError("Failed to find fork in the robot's environment")
+
+	# Get the plate kinbody
+	if plate is None:
+		plate = env.GetKinBody('plate')
+		if plate is None:
+			raise ActionError("Failed to find plate in the robot's environment")
+				
+	# Set default z_offset if not provided
+	if z_offset is None:
+		z_offset = 0.02
+	
+	# Get the manipulator
+	if manip is None:
+		with robot.GetEnv():
+			manip = robot.GetActiveManipulator()
+
+			
+	# fork pointed downward
+	desired_fork_tip_in_world = numpy.array([[-1.,  0., 0., 0.],
+											 [ 0.,  1., 0., 0.],
+											 [ 0.,  0.,-1., 0.],
+											 [ 0.,  0., 0., 1.]])
+	Rz = rotation_matrix(numpy.pi, [0,0,1])
+	desired_fork_tip_in_world = numpy.dot(desired_fork_tip_in_world, Rz)
+	
+	# calculate desired fork position to be above the 
+	z_above_plate = 0.05
+	y_fork_offset = -0.02 # for the bending of the tines
+	plate_pose = plate.GetTransform()
+	desired_fork_tip_in_world[0,3] = plate_pose[0,3] + goal_position[0]
+	desired_fork_tip_in_world[1,3] = plate_pose[1,3] + goal_position[1] + y_fork_offset
+	desired_fork_tip_in_world[2,3] = plate_pose[2,3] + z_offset + z_above_plate
+	
+	# transform desired fork tip into desired end effector pose
+	fork_tip_in_world = fork.GetLink('tinetip').GetTransform()
+	ee_in_world = manip.GetEndEffectorTransform()
+	ee_in_fork_tip = numpy.dot(numpy.linalg.inv(fork_tip_in_world), ee_in_world)
+	desired_ee_pose = numpy.dot(desired_fork_tip_in_world, ee_in_fork_tip)   
+
+	# plan to above the morsal
+	try:
+		with prpy.viz.RenderPoses([fork_tip_in_world, desired_fork_tip_in_world], env):
+			path = robot.PlanToEndEffectorPose(desired_ee_pose, execute=True)
+	except PlanningError, e:
+		raise ActionError('Failed to plan to pose above morsal: %s' % str(e))
+
+	# move downward until reaching the offset provided
+	try:
+		direction = numpy.array([0., 0., -1.])
+		distance = z_above_plate
+		with prpy.viz.RenderVector(manip.GetEndEffectorTransform()[:3,3], direction=direction, length=distance, env=robot.GetEnv()):
+			with prpy.rave.Disabled(fork):
+				T = robot.arm.GetEndEffectorTransform()
+				path = robot.arm.PlanToEndEffectorOffset(direction=direction, distance=distance, execute=True)
+	except PlanningError, e:
+		raise ActionError('Failed to plan straight line path to grab morsal: %s' % str(e))
